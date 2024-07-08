80d67b9f69e5a8a846ff26f5df8bad01f6192734
lucy1287 <lucy1287@naver.com>
2024-07-08 20:45:14 +0900

    Feat: 채팅 보내기

--- app/src/main/java/com/example/a2024_madcamp_week2/ChatActivity.kt
+++ app/src/main/java/com/example/a2024_madcamp_week2/ChatActivity.kt
@@ -25,21 +25,21 @@ class ChatActivity : AppCompatActivity() {
     var tv_text_from_server2: TextView? = null
 
     private lateinit var binding: ActivityChatBinding
+    private var chatRoomId: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityChatBinding.inflate(layoutInflater)
+
+        chatRoomId = intent.getIntExtra("chatRoomId", -1)
+
         btn_conn = binding.btnCon
         btn_send = binding.btnSend
         tv_text_con = binding.tvTextConn //서버로부터 온 클라이언트 id출력
         tv_text_from_server = binding.tvTextFromServer //서버로부터 온 msg출력
         tv_text_from_server2 = binding.tvTextFromServer2 //서버로부터 온 msg2출력
-        btn_conn!!.setOnClickListener(
-            View.OnClickListener
-            //연결버튼 클릭
-            {
-                connect() //연결함수
-            })
+
+        connect() //연결함수
         btn_send!!.setOnClickListener(
             View.OnClickListener
             //보내기버튼 클릭
@@ -54,7 +54,11 @@ class ChatActivity : AppCompatActivity() {
         try {
             mSocket = IO.socket("http://143.248.226.14:3000") //안드로이드 avd사용시 로컬 호스트는 이 주소사용
             mSocket!!.connect() //위 주소로 연결
+
             Log.d("SOCKET", "Connection success : " + mSocket?.id())
+            mSocket?.emit("join_room", chatRoomId)
+            mSocket?.on("joined_room", onJoinedRoom)
+
             mSocket?.on("check_con", Emitter.Listener { args ->
 
                 //서버가 'check_con'이벤트를 일으킨경우
@@ -85,11 +89,13 @@ class ChatActivity : AppCompatActivity() {
         if (mSocket != null) { //연결된경우에만 보내기 가능
             val data = JSONObject() //서버에게 줄 데이터를 json으로 만든다
             try {
-                data.put("id", "test2525") //위에서 만든 json에 키와 값을 넣음
-                data.put("password", "testpass")
+                data.put("room", chatRoomId)
+//                data.put("id", "test2525") //위에서 만든 json에 키와 값을 넣음
+//                data.put("password", "testpass")
+                data.put("message", "하하하")
                 mSocket!!.emit("msg", data) //서버에게 msg 이벤트 일어나게 함
 
-                mSocket!!.on("msg_to_client") { args ->
+                mSocket!!.on("message_received") { args ->
 
                     //서버가 msg_to_client이벤트 일으키면 실행
                     //args에 서버가 보낸 데이터 들어감
@@ -97,9 +103,9 @@ class ChatActivity : AppCompatActivity() {
                         try {
                             val data = args[0] as JSONObject
                             tv_text_from_server!!.text =
-                                data.getString("msg") //서버가 보낸 json에서 msg라는 키의 값만 텍스트뷰에 출력
+                                data.getString("sender") //서버가 보낸 json에서 msg라는 키의 값만 텍스트뷰에 출력
                             tv_text_from_server2!!.text =
-                                data.getString("msg2") //서버가 보낸 json에서 msg2라는 키의 값만 텍스트뷰에 출력
+                                data.getString("message") //서버가 보낸 json에서 msg2라는 키의 값만 텍스트뷰에 출력
                         } catch (e: Exception) {
                             Toast.makeText(
                                 applicationContext, e.message,
@@ -119,9 +125,26 @@ class ChatActivity : AppCompatActivity() {
         }
     }
 
+    private val onJoinedRoom = Emitter.Listener { args ->
+        runOnUiThread {
+            try {
+                val message = args[0] as String
+                Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
+            } catch (e: Exception) {
+                handleException(e)
+            }
+        }
+    }
+
     override fun onDestroy() { //어플리케이션 종료시 실행
         super.onDestroy()
         // mSocket.emit("disconnect",null);
         mSocket!!.disconnect() //소켓을 닫는다
+    }
+
+    private fun handleException(e: Exception) {
+        Toast.makeText(applicationContext, "Error: ${e.message}", Toast.LENGTH_SHORT).show()
+        Log.e("Exception", e.message ?: "Unknown exception")
+        e.printStackTrace()
     }
 }
