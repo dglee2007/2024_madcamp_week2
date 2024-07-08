bfb92a9dd8412930f6a2e2538ff71ff319fa3dbd
lucy1287 <lucy1287@naver.com>
2024-07-08 20:58:57 +0900

    Feat: 채팅 사용자 구분, 메세지 동적 추가

--- app/src/main/java/com/example/a2024_madcamp_week2/ChatActivity.kt
+++ app/src/main/java/com/example/a2024_madcamp_week2/ChatActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -18,110 +20,56 @@ import org.json.JSONObject
 class ChatActivity : AppCompatActivity() {
     private var mSocket: Socket? = null
 
-    var btn_conn: Button? = null
-    var btn_send: Button? = null
-    var tv_text_con: TextView? = null
-    var tv_text_from_server: TextView? = null
-    var tv_text_from_server2: TextView? = null
-
     private lateinit var binding: ActivityChatBinding
+    private lateinit var btnSend: Button
+    private lateinit var etMessage: EditText
+    private lateinit var llMessages: LinearLayout
+
     private var chatRoomId: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityChatBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         chatRoomId = intent.getIntExtra("chatRoomId", -1)
 
-        btn_conn = binding.btnCon
-        btn_send = binding.btnSend
-        tv_text_con = binding.tvTextConn //서버로부터 온 클라이언트 id출력
-        tv_text_from_server = binding.tvTextFromServer //서버로부터 온 msg출력
-        tv_text_from_server2 = binding.tvTextFromServer2 //서버로부터 온 msg2출력
+        btnSend = binding.btnSend
+        etMessage = binding.etMessage
+        llMessages = binding.llMessages
 
-        connect() //연결함수
-        btn_send!!.setOnClickListener(
-            View.OnClickListener
-            //보내기버튼 클릭
-            {
-                send() //보내기함수
-            })
+        connect()
 
-        setContentView(binding.root)
-    }
-
-    fun connect() {
-        try {
-            mSocket = IO.socket("http://143.248.226.14:3000") //안드로이드 avd사용시 로컬 호스트는 이 주소사용
-            mSocket!!.connect() //위 주소로 연결
-
-            Log.d("SOCKET", "Connection success : " + mSocket?.id())
-            mSocket?.emit("join_room", chatRoomId)
-            mSocket?.on("joined_room", onJoinedRoom)
-
-            mSocket?.on("check_con", Emitter.Listener { args ->
-
-                //서버가 'check_con'이벤트를 일으킨경우
-                //args에 서버가 보내온 json데이터 들어간다
-                runOnUiThread {
-                    try {
-                        val data = args[0] as JSONObject
-                        tv_text_con!!.text =
-                            data.getString("id") //json에서 id라는 키의 값만 빼서 텍스트뷰에 출력
-                    } catch (e: Exception) {
-                        Toast.makeText(
-                            applicationContext, e.message,
-                            Toast.LENGTH_LONG
-                        ).show()
-                        e.printStackTrace()
-                    }
-                }
-            })
-        } catch (e: Exception) {
-            tv_text_con!!.text = "안됨"
-            Toast.makeText(applicationContext, e.message, Toast.LENGTH_LONG)
-                .show()
-            Log.e("에러", e.printStackTrace().toString())
+        btnSend.setOnClickListener {
+            val message = etMessage.text.toString().trim()
+            if (message.isNotEmpty()) {
+                sendMessage(message)
+                etMessage.text.clear()
+            } else {
+                Toast.makeText(this, "메시지를 입력하세요.", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
-    fun send() {
-        if (mSocket != null) { //연결된경우에만 보내기 가능
-            val data = JSONObject() //서버에게 줄 데이터를 json으로 만든다
-            try {
-                data.put("room", chatRoomId)
-//                data.put("id", "test2525") //위에서 만든 json에 키와 값을 넣음
-//                data.put("password", "testpass")
-                data.put("message", "하하하")
-                mSocket!!.emit("msg", data) //서버에게 msg 이벤트 일어나게 함
+    private fun connect() {
+        try {
+            mSocket = IO.socket("http://143.248.226.14:3000")
+            mSocket!!.connect()
 
-                mSocket!!.on("message_received") { args ->
+            Log.d("SOCKET", "Connection success : " + mSocket?.id())
+            mSocket?.emit("join_room", chatRoomId)
 
-                    //서버가 msg_to_client이벤트 일으키면 실행
-                    //args에 서버가 보낸 데이터 들어감
-                    runOnUiThread {
-                        try {
-                            val data = args[0] as JSONObject
-                            tv_text_from_server!!.text =
-                                data.getString("sender") //서버가 보낸 json에서 msg라는 키의 값만 텍스트뷰에 출력
-                            tv_text_from_server2!!.text =
-                                data.getString("message") //서버가 보낸 json에서 msg2라는 키의 값만 텍스트뷰에 출력
-                        } catch (e: Exception) {
-                            Toast.makeText(
-                                applicationContext, e.message,
-                                Toast.LENGTH_LONG
-                            ).show()
-                            e.printStackTrace()
-                        }
-                    }
-                }
-            } catch (e: Exception) {
-                Toast.makeText(
-                    applicationContext, e.message, Toast
-                        .LENGTH_LONG
-                ).show()
-                e.printStackTrace()
-            }
+            // 서버에서 'joined_room' 이벤트를 받을 때 호출될 콜백 설정
+            mSocket?.on("joined_room", onJoinedRoom)
+
+            // 서버에서 'message_received' 이벤트를 받을 때 호출될 콜백 설정
+            mSocket?.on("message_received", onMessageReceived)
+
+            // 서버에서 'check_con' 이벤트를 받을 때 호출될 콜백 설정
+            mSocket?.on("check_con", onCheckConnection)
+
+        } catch (e: Exception) {
+            handleException(e)
         }
     }
 
@@ -136,10 +84,73 @@ class ChatActivity : AppCompatActivity() {
         }
     }
 
-    override fun onDestroy() { //어플리케이션 종료시 실행
+    private val onCheckConnection = Emitter.Listener { args ->
+        runOnUiThread {
+            try {
+                val data = args[0] as JSONObject
+                val id = data.getString("id")
+                binding.tvTextConn.text = id
+            } catch (e: Exception) {
+                handleException(e)
+            }
+        }
+    }
+
+    private val onMessageReceived = Emitter.Listener { args ->
+        runOnUiThread {
+            try {
+                val data = args[0] as JSONObject
+                val sender = data.getString("sender")
+                val message = data.getString("message")
+                displayMessage(sender, message)
+            } catch (e: Exception) {
+                handleException(e)
+            }
+        }
+    }
+
+    private fun sendMessage(message: String) {
+        try {
+            val data = JSONObject()
+            data.put("room", chatRoomId)
+            data.put("message", message)
+            mSocket!!.emit("msg", data)
+        } catch (e: Exception) {
+            handleException(e)
+        }
+    }
+
+    private fun displayMessage(sender: String, message: String) {
+        val messageView = TextView(this)
+        messageView.text = "$sender: $message"
+        val params = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        params.setMargins(16, 8, 16, 8)
+
+        // 사용자가 보낸 메시지와 다른 사람이 보낸 메시지를 구분하여 텍스트뷰 정렬
+        if (sender == mSocket!!.id()) {
+            // 사용자가 보낸 메시지 (왼쪽 정렬)
+            //messageView.setBackgroundResource(R.drawable.bg_chat_user)
+            params.marginStart = 64
+            params.marginEnd = 16
+            messageView.textAlignment = View.TEXT_ALIGNMENT_TEXT_START
+        } else {
+            // 다른 사람이 보낸 메시지 (오른쪽 정렬)
+           // messageView.setBackgroundResource(R.drawable.bg_chat_other)
+            params.marginStart = 16
+            params.marginEnd = 64
+            messageView.textAlignment = View.TEXT_ALIGNMENT_TEXT_END
+        }
+
+        messageView.layoutParams = params
+        llMessages.addView(messageView)
+    }
+
+    override fun onDestroy() {
         super.onDestroy()
-        // mSocket.emit("disconnect",null);
-        mSocket!!.disconnect() //소켓을 닫는다
+        mSocket!!.disconnect()
     }
 
     private fun handleException(e: Exception) {
