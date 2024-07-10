d99504938f5ec5e6f16bd043a3b9ba5eac148a2d
lucy1287 <lucy1287@naver.com>
2024-07-10 18:18:57 +0900

    gradle

--- app/src/main/java/com/example/a2024_madcamp_week2/ui/mypage/MypageFragment.kt
+++ app/src/main/java/com/example/a2024_madcamp_week2/ui/mypage/MypageFragment.kt
@@ -11,6 +11,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import com.bumptech.glide.Glide
@@ -18,7 +19,13 @@ import com.example.a2024_madcamp_week2.LogoutActivity
 import com.example.a2024_madcamp_week2.R
 import com.example.a2024_madcamp_week2.api.PlanResponse
 import com.example.a2024_madcamp_week2.api.PlanService
+import com.example.a2024_madcamp_week2.databinding.FragmentMypageBinding
 import com.example.a2024_madcamp_week2.utility.getUserIdFromSharedPreferences
+import com.kakao.vectormap.KakaoMap
+import com.kakao.vectormap.KakaoMapReadyCallback
+import com.kakao.vectormap.KakaoMapSdk
+import com.kakao.vectormap.MapLifeCycleCallback
+import com.kakao.vectormap.MapView
 import com.prolificinteractive.materialcalendarview.CalendarDay
 import com.prolificinteractive.materialcalendarview.DayViewDecorator
 import com.prolificinteractive.materialcalendarview.DayViewFacade
@@ -26,20 +33,35 @@ import com.prolificinteractive.materialcalendarview.MaterialCalendarView
 import com.prolificinteractive.materialcalendarview.spans.DotSpan
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import java.net.URLEncoder
+import kotlin.math.pow
+import kotlin.math.roundToInt
+
 
 class MypageFragment : Fragment() {
 
+    private lateinit var binding: FragmentMypageBinding
     private lateinit var calendarView: MaterialCalendarView
     private val events = mutableSetOf<CalendarDay>()
     private val myPlanList = ArrayList<PlanResponse>()
     private lateinit var planView: LinearLayout // planView LinearLayout 참조
+    private lateinit var mapView: MapView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentMypageBinding.inflate(inflater, container, false)
         val view = inflater.inflate(R.layout.fragment_mypage, container, false)
+
+        KakaoMapSdk.init(requireContext(), "@string/kakao_app_key");
 
         calendarView = view.findViewById(R.id.calendarView)
         planView = view.findViewById(R.id.planView)
@@ -143,4 +165,75 @@ class MypageFragment : Fragment() {
             Log.e(ContentValues.TAG, "Exception occurred: ${e.message}", e)
         }
     }
+
+    fun search(mapSearchInput: String) = runBlocking {
+        val addr: String = URLEncoder.encode(mapSearchInput, "UTF-8")
+
+        // 카카오 REST API 키
+        val apiKey = "@string/kakao_app_key"
+
+        // 검색 API 호출
+        val searchUrl = "https://dapi.kakao.com/v2/local/search/keyword.json?query=$addr"
+        val response = withContext(Dispatchers.IO) { sendGetRequest(searchUrl, apiKey) }
+
+        // 검색 결과 파싱
+        val places = mutableListOf<Pair<String, String>>() // 장소명과 좌표를 저장할 리스트
+        val jsonObject = JSONObject(response)
+        val documents = jsonObject.getJSONArray("documents")
+        for (i in 0 until documents.length()) {
+            val document = documents.getJSONObject(i)
+            val placeName = document.getString("place_name")
+            val x = document.getString("x")
+            val y = document.getString("y")
+            places.add(Pair(placeName, "$x,$y"))
+        }
+
+        // 첫 번째 검색 결과로 지도 이동
+        if (places.isNotEmpty()) {
+            val firstPlace = places[0]
+            val mapUrl = "https://map.kakao.com/link/map/${firstPlace.second}"
+            println("장소명: ${firstPlace.first}")
+            println("좌표: ${firstPlace.second}")
+            println("지도 링크: $mapUrl")
+
+            val coordinates = firstPlace.second.split(",") // 콤마로 분리하여 리스트로 저장
+            val longitude = coordinates[0].toDouble().roundTo(6)// 경도
+            val latitude = coordinates[1].toDouble().roundTo(6) // 위도
+            Log.d("latitude", "latitude" + latitude.toString())
+            Log.d("longitude", "longitude" + longitude.toString())
+
+//            val latLng = LatLng(latitude, longitude) // 위도와 경도를 지정하세요
+//            val cameraPosition = CameraPosition(latLng, 16.0) // 중심점과 줌 레벨을 설정합니다
+//            naverMap.cameraPosition = cameraPosition
+        } else {
+            Toast.makeText(requireContext(), "검색결과가 없습니다. 다시 검색해 주세요", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun sendGetRequest(urlString: String, apiKey: String): String {
+        val url = URL(urlString)
+        val connection = url.openConnection() as HttpURLConnection
+        connection.requestMethod = "GET"
+        connection.setRequestProperty("Authorization", "KakaoAK $apiKey")
+
+        val responseCode = connection.responseCode
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            val reader = BufferedReader(InputStreamReader(connection.inputStream))
+            val response = StringBuffer()
+            var line: String?
+            while (reader.readLine().also { line = it } != null) {
+                response.append(line)
+            }
+            reader.close()
+            return response.toString()
+        } else {
+            throw Exception("HTTP GET request failed with response code: $responseCode")
+        }
+    }
+
+    fun Double.roundTo(decimals: Int): Double {
+        val factor = 10.0.pow(decimals.toDouble())
+        return (this * factor).roundToInt() / factor
+    }
+
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,6 +13,8 @@ dependencyResolutionManagement {
         mavenCentral()
         maven { url = uri("https://devrepo.kakao.com/nexus/content/groups/public/") }
         maven { url  = uri("https://jitpack.io") }
+        maven { url = uri("https://devrepo.kakao.com/nexus/repository/kakaomap-releases/") }
+        maven{ url = uri("https://naver.jfrog.io/artifactory/maven/") }
     }
 }
 
